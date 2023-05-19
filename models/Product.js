23dcd32fc28d0183e9b523054ae4532b58fa6743
ctalv <122413805+ctalv@users.noreply.github.com>
2023-05-19 15:41:48 -0500

    fixed errors in model files

--- models/Product.js
+++ models/Product.js
@@ -28,7 +28,7 @@ Product.init(
       }
     },
     stock: {
-      type: DataTypes.INTERGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
       validate: {
@@ -36,7 +36,7 @@ Product.init(
       }
     },
     category_id: {
-      type: DataTypes.INTERGER,
+      type: DataTypes.INTEGER,
       references: {
         model: 'category',
         key: 'id',
