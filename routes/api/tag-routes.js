66e02c1cd2a1e531806a763d9a546af5509c6d33
ctalv <122413805+ctalv@users.noreply.github.com>
2023-05-21 14:12:44 -0500

    all GET routes created

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,19 +6,31 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  const tagData = await Tag.findAll({
+  try {
+    const tagData = await Tag.findAll({
     include: [{ model: Product }]
   })
-  try {
     res.status(200).json(tagData)
   } catch (err) {
     res.status(500).json(err)
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+    if (!tagData) {
+      res.status(404),json({ message: 'No tag with that id.'})
+      return
+    }
+    res.status(200).json(tagData);
+      } catch(err) {
+    res.status(500).json(err);
+      }
 });
 
 router.post('/', (req, res) => {
