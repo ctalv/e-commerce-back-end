69787c7e855201610ed56de3e7254e8bc75bd042
ctalv <122413805+ctalv@users.noreply.github.com>
2023-05-21 16:34:03 -0500

    added PUT routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -43,8 +43,19 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  const updateTag = await Tag.update (
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  );
+  res.json(updateTag);
 });
 
 router.delete('/:id', (req, res) => {
