0ba571ccd7b43149610c5dac3906cace39d03783
ctalv <122413805+ctalv@users.noreply.github.com>
2023-05-21 10:56:42 -0500

    properly seeded database

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,12 +6,12 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  // try {
-  //   const categoryData = await Location.findAll();
-  //   res.status(200).json(categoryData);
-  // } catch (err) {
-  //   res.status(500).json(err);
-  // }
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
